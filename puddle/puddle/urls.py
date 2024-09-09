69a9bfa4fb9138f76c4d6e3d18bc89126f357d7f
Ali Tahvildari <36967683+alithv@users.noreply.github.com>
2024-09-09 14:15:39 +0200

    Adding detail page and etc

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -1,9 +1,16 @@
+#----------- to see the images for test ------------
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+#----------- do not do above code in production -------------
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from core.views import index , contact
 
 urlpatterns = [
+    path( 'items/', include( 'item.urls')),
     path("contact/" , contact,name="contact"),
     path("", index, name="index"),
-    path("admin/", admin.site.urls),
-]
+    path("admin/", admin.site.urls)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
