9ce99cb990440693c58ba467b0a53fba10bbe40e
Ali Tahvildari <36967683+alithv@users.noreply.github.com>
2024-09-10 10:37:45 +0200

    Fixing urls and base html

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -6,11 +6,9 @@ from django.conf.urls.static import static
 #----------- do not do above code in production -------------
 from django.contrib import admin
 from django.urls import path , include
-from core.views import index , contact
 
 urlpatterns = [
     path( 'items/', include( 'item.urls')),
-    path("contact/" , contact,name="contact"),
-    path("", index, name="index"),
+    path("", include('core.urls')),
     path("admin/", admin.site.urls)
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
