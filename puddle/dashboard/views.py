29a55203ba3277cfe38c8912c196440629423dcc
Ali Tahvildari <36967683+alithv@users.noreply.github.com>
2024-09-12 14:31:46 +0200

    Adding the dashboard

--- puddle/dashboard/views.py
+++ puddle/dashboard/views.py
@@ -9,4 +9,4 @@ from item.models import Item
 def index(request):
     items = Item.objects.filter(created_by = request.user)
 
-    return render('request' , 'dashboard/index.html' , {'items' : items})
+    return render(request ,'dashboard/index.html',  {'items' : items})
--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -10,5 +10,6 @@ from django.urls import path , include
 urlpatterns = [
     path( 'items/', include( 'item.urls')),
     path("", include('core.urls')),
+    path('dashboard/' , include('dashboard.urls')),
     path("admin/", admin.site.urls)
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
