229b4f7174a3c470b3ca549213c9b2648e321e5e
Ali Tahvildari <36967683+alithv@users.noreply.github.com>
2024-09-13 13:56:34 +0200

    Adding the dashboard and item to brows

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render , get_object_or_404 , redirect
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm , EditItemForm
 # Create your views here.
+
+def items(request) : 
+     items = Item.objects.filter(is_sold = False)
+     return render(request , 'item/items.html' ,  {
+      'items' : items   
+     })
+
 def detail(request,  pk):
     item = get_object_or_404(Item , pk = pk)
     related_items = Item.objects.filter(category = item.category, is_sold = False).exclude(pk=pk)[0:3]
@@ -31,4 +38,29 @@ def new(request):
     return render(request ,  'item/form.html' ,
         {'form' :  form ,
          'title' : 'New Item'
+         })
+
+@login_required
+def  delete(request , pk):
+    item = get_object_or_404(Item, pk = pk , created_by=request.user)
+    item.delete() 
+
+    return redirect('dashboard:index')
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk = pk , created_by=request.user)
+
+    if request.method == 'POST':
+         form = EditItemForm(request.POST , request.FILES , instance  = form)
+         if form.is_valid():
+            form.save()
+
+            return redirect('item:detail' , pk = item.id)
+    else :
+        form = EditItemForm(instance=item)
+    return render(request ,  'item/form.html' ,
+        {'form' :  form ,
+         'title' : 'Edit Item'
          })
