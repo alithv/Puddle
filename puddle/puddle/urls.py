32f78e63c352e053ed7c435e43bb0460147163f9
Ali Tahvildari <36967683+alithv@users.noreply.github.com>
2024-09-14 15:18:38 +0200

    Final commit for the end of the project

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path("", include('core.urls')),
     path('dashboard/' , include('dashboard.urls')),
     path('inbox/' , include('conversation.urls')),
-    path("admin/", admin.site.urls)
+    path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
